d0e284d0a26022c0df6490bedb654c683d24aa6b
Stéphane Cerveau <scerveau@igalia.com>
2022-11-16 15:11:28 +0100

    gettext: remove crate use because unsupported on Windows.
    Part of #12

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -9,7 +9,6 @@
 use crate::app::GPSApp;
 use crate::config;
 use crate::gps as GPS;
-use gettextrs::gettext;
 use gtk::prelude::*;
 use gtk::AboutDialog;
 
@@ -26,13 +25,13 @@ pub fn display_about_dialog(app: &GPSApp) {
         .version(config::VERSION)
         .comments(format!(
             "{}\n\n{}",
-            &gettext("Draw your own GStreamer pipeline"),
+            &"Draw your own GStreamer pipeline",
             GPS::Player::get_version()
         ))
         .website("https://gitlab.freedesktop.org/dabrain34/GstPipelineStudio")
         .authors(vec!["Stéphane Cerveau".to_string()])
         .artists(vec!["Stéphane Cerveau".to_string()])
-        .translator_credits(gettext("translator-credits"))
+        .translator_credits("translator-credits")
         .logo_icon_name(config::APP_ID)
         .license_type(gtk::License::Gpl30)
         .transient_for(&window)
