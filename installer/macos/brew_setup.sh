5de51d55651b55f8e02ef5cfca8554d58eac2955
Stéphane Cerveau <scerveau@igalia.com>
2023-06-13 16:30:43 +0200

    meson: add gtk4 as a subproject
    Add gtk.wrap to subprojects
    
    Remove gtk4 from brew_setup.sh

--- installer/macos/brew_setup.sh
+++ installer/macos/brew_setup.sh
@@ -4,7 +4,11 @@ HOMEBREW_NO_INSTALL_CLEANUP=1
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install pkg-config gtk4
+brew install pkg-config
+
+# GTK4 support
+brew install gtk4
+# brew install cairo libxrandr libxi libxcursor libxdamage libxinerama
 
 brew install npm
 
