17321d4ebfe8d44ee3ae6b6af444ff8f8b7bb32c
Stéphane Cerveau <scerveau@igalia.com>
2025-11-07 18:05:43 +0100

    ui: replace deprecated MessageDialog with AlertDialog
    Migrate gtk4::MessageDialog to gtk4::AlertDialog in src/ui/message.rs
    to eliminate GTK4 deprecation warnings and modernize dialog handling.

--- src/ui/message.rs
+++ src/ui/message.rs
@@ -9,7 +9,7 @@
 use gtk::gio;
 use gtk::prelude::*;
 
-use gtk::{Application, Label, Widget};
+use gtk::{AlertDialog, Application};
 
 pub fn display_message_dialog<F: Fn(Application) + 'static>(
     message: &str,
@@ -21,44 +21,36 @@ pub fn display_message_dialog<F: Fn(Application) + 'static>(
         .downcast::<gtk::Application>()
         .expect("Default application has wrong type");
 
-    let dialog = gtk::MessageDialog::new(
-        app.active_window().as_ref(),
-        gtk::DialogFlags::MODAL,
-        message_type,
-        gtk::ButtonsType::Ok,
-        message,
-    );
-    let message_area = dialog.message_area();
-    let mut child = message_area.first_child();
-    while child.is_some() {
-        let widget = child.unwrap();
-        let label = widget
-            .dynamic_cast::<Label>()
-            .expect("unable to cast child to Label");
-        label.set_selectable(true);
-        let widget = label.dynamic_cast::<Widget>().unwrap();
-        child = widget.next_sibling();
-    }
+    // AlertDialog doesn't have built-in message types, so format the message
+    let formatted_message = match message_type {
+        gtk::MessageType::Error => format!("Error: {message}"),
+        gtk::MessageType::Warning => format!("Warning: {message}"),
+        gtk::MessageType::Info => format!("Information: {message}"),
+        _ => message.to_string(),
+    };
+
+    let dialog = AlertDialog::builder()
+        .message(&formatted_message)
+        .modal(true)
+        .buttons(["Ok"])
+        .default_button(0)
+        .build();
 
     let app_weak = app.downgrade();
-    dialog.connect_response(move |dialog, _| {
-        let app = upgrade_weak!(app_weak);
-        dialog.destroy();
-        f(app);
-    });
-    dialog.set_resizable(false);
-    dialog.present();
-}
-
-#[allow(dead_code)]
-pub fn display_error_dialog(fatal: bool, message: &str) {
-    display_message_dialog(
-        &format!("Error: {message}"),
-        gtk::MessageType::Error,
-        move |app| {
-            if fatal {
-                app.quit();
-            }
+    dialog.choose(
+        app.active_window().as_ref(),
+        gio::Cancellable::NONE,
+        move |_result| {
+            let app = upgrade_weak!(app_weak);
+            f(app);
         },
     );
+}
+
+pub fn display_error_dialog(fatal: bool, message: &str) {
+    display_message_dialog(message, gtk::MessageType::Error, move |app| {
+        if fatal {
+            app.quit();
+        }
+    });
 }
